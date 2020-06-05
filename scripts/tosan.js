45beea9946ad05ca99a54a87e2742311dbac3555
tosanomagbemi <tosanomagbemi42@gmail.com>
2020-06-05 19:37:23 +0100

    updated commit
    Tosan Omagbemi task 2

--- scripts/tosan.js
+++ scripts/tosan.js
@@ -1,12 +1,6 @@
-tosan = () => {
-    var name, id, language, email;
-    name = "Tosan Omagbemi";
-    id = "HNG-03253";
-    email = "tosanomagbemi42@gmail.com";
-    language = "javascript";
-    return (
-        `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
-    )
-}
-
-console.log(olabode());
+ 
+var name ="Tosan Omagbemi";
+var email="tosanomagbeni42@gmail.com";
+var  language="javascript";
+var id="HNG-03253";
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
